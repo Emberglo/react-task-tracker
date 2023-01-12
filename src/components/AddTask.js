743ec71b25ca35d36199cdb879f7cb5580fd35a9
Ryan Carpenter <rccarpenter@outlook.com>
2023-01-12 13:07:47 -0700

    add more functionality

---
+++ src/components/AddTask.js
@@ -0,0 +1,67 @@
+import { useState } from "react"
+
+const AddTask = ({ onAdd }) => {
+    const [text, setText] = useState('');
+    const [day, setDay] = useState('');
+    const [reminder, setReminder] = useState(false);
+
+    const onSubmit = (e) => {
+        // NOTE - prevent page change/refresh
+        e.preventDefault();
+
+        // NOTE - prevents submitting form without a Task
+        if(!text) {
+            alert('Please Add Text');
+            return
+        }
+
+        // NOTE - call the addTask function in app.js with the informtation from the form
+        onAdd({ text, day, reminder });
+
+        // NOTE - clear form after it's submitted
+        setText('');
+        setDay('');
+        setReminder(false);
+    }
+
+  return (
+    <form className="add-task" onSubmit={onSubmit}>
+        <div className="form-control">
+            <label>Task</label>
+            <input 
+                type="text" 
+                placeholder="Add Text"
+                // NOTE - value is state value and onchange updates the state value with inputs value
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+        </div>
+
+        <div className="form-control">
+            <label>Day & Time</label>
+            <input 
+                type="text" 
+                placeholder="Add Day And Time" 
+                value={day}
+                onChange={(e) => setDay(e.target.value)} 
+            />
+        </div>
+
+        <div className="form-control form-control-check">
+            <label>Set Reminder</label>
+            <input 
+                type="checkbox" 
+                // NOTE - makes sure the checkbox is only checked if reminder is true
+                checked={reminder}
+                value={reminder}
+                // NOTE - currentTarget.checked gives current true/false value of checkbox
+                onChange={(e) => setReminder(e.currentTarget.checked)}
+            />
+        </div>
+
+        <input type="submit" value="Save Task" className="btn btn-block" />
+    </form>
+  )
+}
+
+export default AddTask
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,15 @@
-const Task = ({ task }) => {
+import { FaTimes } from 'react-icons/fa'
+
+const Task = ({ task, onDelete, toggleReminder }) => {
     
     return (
-      <div className="task">
-        <h3>{task.text}</h3>
+        // NOTE - using curly braces and backticks for an expression and the ${} for a condition 
+      <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => toggleReminder(task.id)}>
+        <h3>
+            {task.text}
+            {/* NOTE - onDelete event is passed back up from here to the function in app.js & calling a function in onclick allows us to send just the ID back up otherwise it sends the whole click event */}
+            <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(task.id)} />
+        </h3>
         <p>{task.day}</p>
       </div>
     )
